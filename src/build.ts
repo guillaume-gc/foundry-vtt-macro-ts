e2e8358cac599100eacab6eed5fa5a30d3c00f4d
Guillaume C <cguillaume4@gmail.com>
2023-08-17 08:08:39 +0200

    chore(all): basic tooling

---
+++ src/build.ts
@@ -0,0 +1,41 @@
+import * as esbuild from 'esbuild'
+import * as fs from 'fs'
+import * as path from 'path'
+
+const srcDir = __dirname
+const distDir = path.resolve(__dirname, '..', 'dist')
+
+// Ensure the dist directory exists.
+if (!fs.existsSync(distDir)) {
+  fs.mkdirSync(distDir)
+}
+
+// Get all subdirectories in the src directory.
+const getDirectories = (source: string) =>
+  fs
+    .readdirSync(source, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name)
+
+const subDirs = getDirectories(srcDir)
+
+// Build each subdirectory.
+subDirs.forEach((dir) => {
+  const entryFile = path.join(srcDir, dir, 'index.ts')
+  const outputDir = path.join(distDir, dir)
+
+  if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir)
+  }
+
+  esbuild
+    .build({
+      entryPoints: [entryFile],
+      bundle: true,
+      outfile: path.join(outputDir, 'index.js'),
+      platform: 'node', // If it's for the browser, change this to 'browser'
+      target: 'es2020', // Change this to your desired target.
+      loader: { '.ts': 'ts' },
+    })
+    .catch((error) => console.error(error))
+})
